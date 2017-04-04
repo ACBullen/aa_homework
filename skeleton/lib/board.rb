4cc61833b66a2d1bec0cd28834fd4b01753a2df6
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-04-03 21:25:33 -0700

    Set one_side_empty?, begin winner assignment

--- skeleton/lib/board.rb
+++ skeleton/lib/board.rb
@@ -1,11 +1,13 @@
+require 'byebug'
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
     @player1 = name1
     @player2 = name2
+    @current_player = @player1
     @cups =  Array.new(14){ [] }
-
+    place_stones
   end
 
   def place_stones
@@ -15,6 +17,7 @@ class Board
         4.times do
           cup << :stone
         end
+
       end
     end
   end
@@ -24,10 +27,33 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+  #  debugger
+    idx = start_pos
+    until cups[start_pos].length == 0
+      idx += 1
+      idx == 0 if idx > 13
+      if idx == 6
+        idx = 7 unless @player1 == current_player_name
+      end
+      if idx == 13
+        idx = 0 unless @player2 == current_player_name
+      end
+      cups[start_pos]= cups[start_pos][0...-1]
+      cups[idx] << :stone
+    end
+    render
+    next_turn(idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif cups[ending_cup_idx] == :stone
+      :switch
+    else
+    ending_cup_idx
+    end
   end
 
   def render
@@ -39,8 +65,25 @@ class Board
   end
 
   def one_side_empty?
+    first_side_empty = false
+    second_side_empty = false
+    first_side_empty = true if cups[0..5].all? {|cup| cup.empty?}
+    second_side_empty = true if cups[7..12].all? { |cup| cup.empty?}
+    if first_side_empty || second_side_empty
+      true
+    else
+      false
+    end
   end
 
   def winner
+    case cups[6] <=> cups[13]
+    when -1
+
+    when 0
+      :draw
+    when 1
+
+    end
   end
 end
