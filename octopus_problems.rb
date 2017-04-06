d22cb7c4186b2ab283d392e178a900ef4cb69126
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-04-05 19:56:57 -0700

    Rename sluggish to clever as that is what was actually implemented then

--- octopus_problems.rb
+++ octopus_problems.rb
@@ -1,5 +1,5 @@
 fish_array = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
-def sluggish_octopus_search(array)
+def clever_octopus_search(array)
   longest = array[0]
   i = 0
   while i < array.length
