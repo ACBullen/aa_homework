4a8b7796057c0a7daf296005b6d8d87fe2759f61
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-04-06 20:57:10 -0700

    create lru cache exercise file

--- octopus_problems.rb
+++ octopus_problems.rb
@@ -28,3 +28,59 @@ def dominant_octopus_search(array)
   end
   array.last
 end
+
+def sluggish_octopus_search(array)
+  array.each_with_index do |fish, idx|
+    max_length = true
+    array.each_with_index do |fish2, idx2|
+      next if idx1 == idx2
+      max_length = false if fish2.length > fish.length
+    end
+    return fish if max_length
+  end
+end
+class Array
+  def merge_sort(&prc)
+    return self if array.length <= 1
+    prc ||= Proc.new { |x, y| x <=> y }
+    sorted_left = self[0...mid].merge_sort(&prc)
+    sorted_right = self[mid..-1].merge_sort(&prc)
+
+    Array.merge(sorted_left, sorted_right)
+  end
+  private
+  def self.merge(left, right, &prc)
+    sorted = []
+    until left.empty? || right.empty?
+      case prc.call(left.first, right.first)
+      when -1
+        sorted << left.shift
+      when 0
+        sorted << left.shift
+      when 1
+        sorted << right.shift
+      end
+    end
+    sorted.concat(left)
+    sorted.concat(right)
+
+    sorted
+  end
+end
+
+def actual_dominant_octopus(array)
+  sorted = array.merge_sort
+
+  sorted.last
+end
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+def slow_dance(direction, array)
+  array.each_with_index do |el, idx|
+    return idx if el == direction
+  end
+end
+tiles_hash = {"up" => 0, "right-up" => 1, "right" => 2, "right-down" => 3, "down" => 4, "left-down" => 5, "left" => 6, "left-up", 7}
+
+def continuous_dance(direction, new_tiles_data_structure)
+  new_tiles_data_structure[direction]
+end
