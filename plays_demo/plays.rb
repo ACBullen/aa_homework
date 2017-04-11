a8c50a16f1d34e34eadff5cb4744c196c8cb08c5
Alexander Bullen <alexmain@Alexanders-MacBook-Pro.local>
2017-04-10 20:57:22 -0700

    Create factory method for turning db items into class instances and initialize appropriately

--- plays_demo/plays.rb
+++ plays_demo/plays.rb
@@ -72,7 +72,21 @@ class Play
         name = ?
 
       SQL
-    end
-
   end
+
+end
+
+class Playwright
+
+  def self.all
+    data = PlayDBConnection.instance.execute("Select * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
 end
